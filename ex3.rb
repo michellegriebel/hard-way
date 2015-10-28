b8ca35f98633840804afcc381d77d38a53578240
Michelle Griebel <mgriebel@enventis.net>
2015-10-28 20:51:36 +0000

    initial commit, 1-5 done

---
+++ ex3.rb
@@ -0,0 +1,42 @@
+# displays the string
+puts "I will now count my chickens:"
+
+# displays Hens then divides 30/6 and then adds 25 to display 30
+puts "Hens #{25.0 + 30.0 / 6.0}"
+
+# displays Roosters 97
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# displays the string
+puts "Now I will count the eggs:"
+
+# displays 7 but then after using floating numbers the value changes to 6.75
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# displays the string
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# displays false
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# displays "What is 3 + 2? 5.0"
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+
+# displays "What is 5 - 7? -2"
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+# displays the string
+puts "Oh, that's why it's false."
+
+# dislays the string
+puts "How about some more."
+
+# displays "Is it greater? true"
+puts "Is it greater? #{5.0 > -2.0}"
+
+# displays "Is it greater or equal? true"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+
+# displays "Is it less or equal? false"
+puts "Is it less or equal? #{5.0 <= -2.0}"
+
---
+++ ruby ex5.rb
@@ -0,0 +1,3 @@
+
+my_name = 'Zed A. Shaw'
+
