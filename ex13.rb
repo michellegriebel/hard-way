2a58c97cd6ebdf6aef4035744fea7926c6a97b1d
Michelle Griebel <mgriebel@enventis.net>
2015-11-09 02:03:31 +0000

    Add exercise 11

---
+++ ex13.rb
@@ -0,0 +1,17 @@
+# first, second, third = ARGV
+
+print "What is your first variable? "
+first = gets.chomp
+
+print "What is your second variable? "
+second = gets.chomp
+
+print "What is your third variable? "
+third = gets.chomp
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+
+
