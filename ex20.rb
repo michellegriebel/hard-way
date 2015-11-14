9fd854e7290b3dc2f22f0d7cdcbbe433fb9e5fb8
Michelle Griebel <mgriebel@enventis.net>
2015-11-14 17:54:18 +0000

    Adding Exercises 20-21

--- ex20.rb
+++ ex20.rb
@@ -1,21 +1,35 @@
+# Line 2 uses ARGV to get a filename
 input_file = ARGV.first
 
+# defines the function print_all with f as the variable
 def print_all(f)
+# reads from f
   puts f.read
 end
 
+# defines the function rewind with the f as the variable
 def rewind(f)
+
+# seeks from f variable or File#seek to randomly access the file. 
+  # ri "File#seek"
+# moving to the start of the file 
+# seek() function is dealing in bytes, not lines. The code seek(0) moves the file to the 0 byte (first byte) in the file. 
   f.seek(0)
 end
 
+# defines the function print_a_line with two arguments(line_count, f)
 def print_a_line(line_count, f)
+  
+# displays line_count and gets the input from the File 1, 2, 3,.
   puts "#{line_count}, #{f.gets.chomp}"
 end
 
+# open the file and assigns it to the current_file variable
 current_file = open(input_file)
 
 puts "First let's print the whole file:\n"
 
+# Prints everything from the file.
 print_all(current_file)
 
 puts "Now let's rewind, kind of like a tape."
@@ -24,11 +38,14 @@ rewind(current_file)
 
 puts "Let's print three lines:"
 
+# manually incrementing 1 to the the current_line variable
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+# Adds 1 to the current_line variable to equal 2
+current_line = current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+# Adds 1 to the current_line variable to equal 3
+current_line = current_line += 1
 print_a_line(current_line, current_file)
---
+++ ex21.rb
@@ -0,0 +1,39 @@
+# Our function is called with two arguments: a and b.
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+  return a + b
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+  return a - b
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+  return a * b
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+  return a / b
+end
+
+
+puts "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+
+# A puzzle for the extra credit, type it in anyway.
+
+puts "Here is a puzzle."
+# I'm taking the return value of one function and using it as the argument of another function. 
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes: #{what}. Can you do it by hand?"
